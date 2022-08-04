16a06ff25c7f66d0919aa8d9d393fb781bd964cb
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-08-04 16:58:40 -0500

    Fourth workshop: 'GET: leyendo michis favoritos'

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-const API = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=6ef2cd16-4ef8-44c0-9bfe-78a44da2df70"
 const catImage = document.querySelector(".cat_image")
 const catImage2 = document.querySelector(".cat_image2")
-const catImage3 = document.querySelector(".cat_image3")
 const catButton = document.querySelector(".cat_button")
+const spanError = document.getElementById("error")
 
-async function randomCat(){
-    const response = await fetch(API)
+const API = "https://api.thecatapi.com/v1"
+
+const API_KEY = "6ef2cd16-4ef8-44c0-9bfe-78a44da2df70"
+
+
+async function loadRandomMichis(){
+    const response = await fetch(`${API}/images/search?limit=2&api_key=${API_KEY}`)
     const data = await response.json()
 
-    catImage.src = data[0].url
-    catImage2.src = data[1].url
-    catImage3.src = data[2].url
+    if (response.status != 200) {
+        spanError.innerHTML = "Hubo un error: " + response.status
+    }else{
+        catImage.src = data[0].url
+        catImage2.src = data[1].url
+    }
 }
-randomCat();
-catButton.addEventListener("click", randomCat);
+catButton.addEventListener("click", loadRandomMichis);
+
+async function loadFavoriteMichis(){
+    const response = await fetch(`${API}/favourites?api_key=${API_KEY}`)
+    const data = await response.json()
+
+    if(response.status != 200){
+        spanError.innerHTML = "Hubo un error: " + response.status + data.message
+    }else{
+        console.log(data)
+        
+    }
+}
+
+loadRandomMichis();
+loadFavoriteMichis();
+
