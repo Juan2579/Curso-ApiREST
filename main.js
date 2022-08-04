381eef77cf9cacd9d3815f875866516ab1001be6
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-08-04 11:12:55 -0500

    Second workshop: 'Endpoints y query parameters'

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const API = "https://api.thecatapi.com/v1/images/search"
+const API = "https://api.thecatapi.com/v1/images/search?limit=3"
 const catImage = document.querySelector(".cat_image")
+const catImage2 = document.querySelector(".cat_image2")
+const catImage3 = document.querySelector(".cat_image3")
 const catButton = document.querySelector(".cat_button")
 
 async function randomCat(){
@@ -7,6 +9,8 @@ async function randomCat(){
     const data = await response.json()
 
     catImage.src = data[0].url
+    catImage2.src = data[1].url
+    catImage3.src = data[2].url
 }
 randomCat();
 catButton.addEventListener("click", randomCat);
