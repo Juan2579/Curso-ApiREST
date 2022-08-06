054fa2e9aae3c095b666f659f0e58cdb891e2303
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-08-06 15:46:26 -0500

    Initial structure of: 'DELETE: Borrando michis favoritos'

--- main.js
+++ main.js
@@ -15,6 +15,10 @@ async function loadRandomMichis(){
     if (response.status != 200) {
         spanError.innerHTML = "Hubo un error: " + response.status
     }else{
+        const btn1 = document.getElementById('btn1')
+        const btn2 = document.getElementById('btn2')
+        btn1.onclick = () => saveFavouriteMichi(data[0].id);
+        btn2.onclick = () => saveFavouriteMichi(data[1].id);
         catImage.src = data[0].url
         catImage2.src = data[1].url
     }
@@ -28,32 +32,57 @@ async function loadFavouriteMichis(){
     if(response.status != 200){
         spanError.innerHTML = "Hubo un error: " + response.status + data.message
     }else{
+        const h2 = document.createElement("h2")
+        const h2Text = document.createTextNode("Michis favoritos")
+        h2.appendChild(h2Text)
         data.forEach(michi => {
-            const section = document.getElementById("favouritesMichis")
+            const section = document.getElementById("favouriteMichis")
             const article = document.createElement("article")
             const img = document.createElement("img")
             const btn = document.createElement("button")
             const btnText = document.createTextNode("Sacar al michi de favoritos")
-            // michi.image.url
+
+            btn.appendChild(btnText)
+            btn.onclick = () => deleteFavouriteMichi(michi.id)
+            img.src = michi.image.url
+
+            article.appendChild(img)
+            article.appendChild(btn)
+            section.appendChild(article)
         })
         
     }
 }
 
-async function saveFavouriteMichis(){
+async function saveFavouriteMichi(id){
     const response = await fetch(`${API}/favourites?api_key=${API_KEY}`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            image_id: "djn"
+            image_id: id
         })
     })
     const data = await response.json();
     console.log(response)
     if(response.status != 200){
         spanError.innerHTML = "Hubo un error: " + response.status + data.message
+    }else{
+        console.log("Michi guardado en favoritos")
+    }
+}
+
+async function deleteFavouriteMichi(id){
+    const response = await fetch(`${API}/favourites/${id}?api_key=${API_KEY}`, {
+        method: 'DELETE',
+        
+    })
+    const data = await response.json();
+    if(response.status != 200){
+        spanError.innerHTML = "Hubo un error: " + response.status + data.message
+    }else{
+        console.log("Michi eliminado de favoritos")
     }
 }
 
