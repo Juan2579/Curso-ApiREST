cc1d23f8a45d78608927cb1476903c1d664516e8
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-08-07 04:04:50 -0500

    Final commit with all the rest classes

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const apiAxios = axios.create({
+    baseURL: "https://api.thecatapi.com/v1"
+})
+apiAxios.defaults.headers.common["x-api-key"] = "6ef2cd16-4ef8-44c0-9bfe-78a44da2df70"
+
 const catImage = document.querySelector(".cat_image")
 const catImage2 = document.querySelector(".cat_image2")
 const catButton = document.querySelector(".cat_button")
@@ -26,17 +31,23 @@ async function loadRandomMichis(){
 catButton.addEventListener("click", loadRandomMichis);
 
 async function loadFavouriteMichis(){
-    const response = await fetch(`${API}/favourites?api_key=${API_KEY}`)
+    const response = await fetch(`${API}/favourites`, {
+        method: "GET",
+        headers: {
+            "x-api-key": API_KEY
+        }
+    })
     const data = await response.json()
 
     if(response.status != 200){
         spanError.innerHTML = "Hubo un error: " + response.status + data.message
     }else{
+        const section = document.getElementById("favouriteMichis")
+        section.innerHTML = ""
         const h2 = document.createElement("h2")
         const h2Text = document.createTextNode("Michis favoritos")
         h2.appendChild(h2Text)
         data.forEach(michi => {
-            const section = document.getElementById("favouriteMichis")
             const article = document.createElement("article")
             const img = document.createElement("img")
             const btn = document.createElement("button")
@@ -55,34 +66,63 @@ async function loadFavouriteMichis(){
 }
 
 async function saveFavouriteMichi(id){
-    const response = await fetch(`${API}/favourites?api_key=${API_KEY}`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            image_id: id
-        })
+    const {data, status} = await apiAxios.post("/favourites",{
+        image_id: id
     })
-    const data = await response.json();
-    console.log(response)
-    if(response.status != 200){
-        spanError.innerHTML = "Hubo un error: " + response.status + data.message
+    // const response = await fetch(`${API}/favourites`, {
+    //     method: 'POST',
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "x-api-key": API_KEY
+    //     },
+    //     body: JSON.stringify({
+    //         image_id: id
+    //     })
+    // })
+    // const data = await response.json();
+    console.log(data)
+    if(status != 200){
+        spanError.innerHTML = "Hubo un error: " + status
     }else{
         console.log("Michi guardado en favoritos")
+        loadFavouriteMichis();
     }
 }
 
 async function deleteFavouriteMichi(id){
-    const response = await fetch(`${API}/favourites/${id}?api_key=${API_KEY}`, {
+    const response = await fetch(`${API}/favourites/${id}`, {
         method: 'DELETE',
-        
+        headers: {
+            "x-api-key": API_KEY,
+        }
     })
     const data = await response.json();
     if(response.status != 200){
         spanError.innerHTML = "Hubo un error: " + response.status + data.message
     }else{
         console.log("Michi eliminado de favoritos")
+        loadFavouriteMichis();
+    }
+}
+
+async function uploadMichiPhoto(){
+    const form = document.getElementById("uploadingForm")
+    const formData = new FormData(form)
+
+    const response = await fetch(`${API}/images/upload`, {
+        method: "POST",
+        headers: {
+            "x-api-key": API_KEY,
+        },
+        body: formData,
+    })
+    const data = await response.json()
+    if(response.status != 201){
+        spanError.innerHTML = "Hubo un error: " + response.status + data.message
+    }else{
+        console.log("Foto de michi guardada")
+        console.log(data)
+        saveFavouriteMichi(data.id)
     }
 }
 
